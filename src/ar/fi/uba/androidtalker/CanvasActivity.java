8c1286dacb2babb1b212bcd4d519cfa80e09d540
sergio <sorsergios@gmail.com>
2014-12-13 13:56:54 -0300

    merge 2

--- src/ar/fi/uba/androidtalker/CanvasActivity.java
+++ src/ar/fi/uba/androidtalker/CanvasActivity.java
@@ -3,6 +3,7 @@ package ar.fi.uba.androidtalker;
 import android.R;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
