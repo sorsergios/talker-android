05af3ee8002cb09022c879e0d1c7f0e776d8b9e9
sergio <sorsergios@gmail.com>
2015-03-07 02:15:09 -0300

    muevo texto al tocarlo.

--- src/ar/uba/fi/talker/component/Component.java
+++ src/ar/uba/fi/talker/component/Component.java
@@ -1,83 +1,42 @@
 package ar.uba.fi.talker.component;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.Point;
-import android.text.Editable;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
-import ar.uba.fi.talker.paint.PaintManager;
-import ar.uba.fi.talker.paint.PaintType;
 
 public abstract class Component extends View {
 	
-	protected Dimension dimension;
 	private boolean active = true;
-	private static int TOLERANCE = 10;
+	private Rect bounds;
 	
 	public Component(Context context) {
 		super(context);
-		dimension = new Dimension();
+		bounds = new Rect();
 	}
 
 	@Override
 	public boolean dispatchTouchEvent(MotionEvent event) {
-		
 		return active && super.dispatchTouchEvent(event);
 	}
 
+	protected Rect getBounds() {
+		return bounds;
+	}
+	
+	public boolean isPointInnerBounds(Point point) {
+		return false;
+	}
+	
 	@Override
 	public boolean performClick() {
 		return active && super.performClick();
 	}
 	
-	public boolean isInDimensions(Point erasePoint) {
-		return erasePoint != null 
-				&& dimension.x1-Component.TOLERANCE <= erasePoint.x 
-				&& dimension.y1-Component.TOLERANCE <= erasePoint.y 
-				&& dimension.x2+Component.TOLERANCE >= erasePoint.x 
-				&& dimension.y2+Component.TOLERANCE >= erasePoint.y;
-	}
-
-	public void drawDimension(Canvas canvas) {
-		Path path = new Path();
-		Paint paint = PaintManager.getPaint(PaintType.ERASE);
-		path.moveTo(dimension.x1, dimension.y1);
-		path.lineTo(dimension.x2, dimension.y1);
-		path.lineTo(dimension.x2, dimension.y2);
-		path.lineTo(dimension.x1, dimension.y2);
-		path.lineTo(dimension.x1, dimension.y1);
-		canvas.drawPath(path, paint);
-	}
-	
-	class Dimension {
-		public int x1 = -1, y1 = -1, x2, y2;
-		
-		public void evalPoint(Point point) {
-			if (x1 == -1 || point.x < x1) {
-				x1 = point.x - Component.TOLERANCE;
-			}
-			if (point.x > x2) {
-				x2 = point.x + Component.TOLERANCE;
-			}
-			
-			if (y1 == -1 || point.y < y1) {
-				y1 = point.y - Component.TOLERANCE;
-			}
-			if (point.y > y2) {
-				y2 = point.y + Component.TOLERANCE;
-			}
-		}
-	}
-	
 	public void toggleActive() {
 		active = !active;
-	}
-
-	public void setValue(Editable text) {
-		// anything to do;
+		this.bringToFront();
 	}
 
 }
--- src/ar/uba/fi/talker/component/Text.java
+++ src/ar/uba/fi/talker/component/Text.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.text.Editable;
 import android.view.MotionEvent;
 import ar.uba.fi.talker.paint.PaintManager;
@@ -60,6 +61,7 @@ public class Text extends Component {
 			point.y += deltaPoint.y;
 			deltaPoint.x = 0;
 			deltaPoint.y = 0;
+			this.toggleActive();
 			break;
 		}
 		
@@ -67,10 +69,21 @@ public class Text extends Component {
 		return this.performClick();
 	}
 
-	@Override
 	public void setValue(Editable text) {
 		this.value = text.toString();
+		paint.getTextBounds(this.value, 0, this.value.length(), this.getBounds());
 		this.invalidate();
 	}
 
+	@Override
+	public boolean isPointInnerBounds(Point outerPoint) {
+		Rect bounds = this.getBounds();
+		
+		return (point.x < outerPoint.x 
+				&& point.y > outerPoint.y
+				&& point.x + bounds.right > outerPoint.x
+				&& point.y + bounds.top < outerPoint.y
+		);
+			
+	}
 }
