4e69a2947f1bcb27cafb4c4cdb4489ea113ac17c
Sergio Soria <sorsergios@gmail.com>
2015-03-28 17:44:59 -0300

    Inserto imagen desde Categoría.

--- src/ar/uba/fi/talker/fragment/InsertImageDialogFragment.java
+++ src/ar/uba/fi/talker/fragment/InsertImageDialogFragment.java
@@ -5,12 +5,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +16,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ViewFlipper;
 import ar.uba.fi.talker.R;
 import ar.uba.fi.talker.adapter.InsertImageCategoryAdapter;
 
@@ -26,11 +25,16 @@ public class InsertImageDialogFragment extends DialogFragment {
 	private static int RESULT_LOAD_IMAGE = 1;
 
 	public interface InsertImageDialogListener {
-		public void onDialogPositiveClickInsertImageDialogListener(Uri uri, Matrix matrix);
+		public void onDialogPositiveClickInsertImageDialogListener(Uri uri,
+				Matrix matrix);
+
 		public void onDialogPositiveClickInsertImageDialogListener(Bitmap bitmap);
 	}
 
-	static final String[] CATEGORIES = new String[] { "ANIMAL", "COMIDA", "OBJETOS", "PATIO", "NUEVA" };
+	static final String[] CATEGORIES = new String[] { "ANIMAL", "COMIDA",
+			"OBJETOS", "PATIO", "NUEVA" };
+	static final String[] INNER_CATEGORIES = new String[] { "IMAGEN 1",
+			"IMAGEN 2", "IMAGEN 3", "IMAGEN 4", "NUEVA" };
 
 	InsertImageDialogListener listener;
 
@@ -51,46 +55,81 @@ public class InsertImageDialogFragment extends DialogFragment {
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
 		// TODO arranca codigo prueba
-//		Button buttonLoadImage = new Button(getActivity());
-//		buttonLoadImage.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View arg0) {
-//
-//				Intent i = new Intent(
-//						Intent.ACTION_PICK,
-//						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//				startActivityForResult(i, RESULT_LOAD_IMAGE);
-//			}
-//		});
+		// Button buttonLoadImage = new Button(getActivity());
+		// buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+		// @Override
+		// public void onClick(View arg0) {
+		//
+		// Intent i = new Intent(
+		// Intent.ACTION_PICK,
+		// android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		// startActivityForResult(i, RESULT_LOAD_IMAGE);
+		// }
+		// });
 
 		LayoutInflater inflater = getActivity().getLayoutInflater();
-		View gridViewContainer = inflater.inflate(R.layout.insert_image_gridview, null);
+		View gridViewContainer = inflater.inflate(
+				R.layout.insert_image_gridview, null);
 
-		GridView gridView = (GridView) gridViewContainer.findViewById(R.id.insert_image_gridview);
-		
-		gridView.setAdapter(new InsertImageCategoryAdapter(getActivity(), CATEGORIES));
- 
+		final ViewFlipper flipper = (ViewFlipper) gridViewContainer
+				.findViewById(R.id.vfImages);
+		GridView gridView = (GridView) gridViewContainer
+				.findViewById(R.id.insert_cat_gridview);
+		GridView gridViewImages = (GridView) gridViewContainer
+				.findViewById(R.id.insert_image_gridview);
+
+		gridView.setAdapter(new InsertImageCategoryAdapter(getActivity(),
+				CATEGORIES));
+		gridViewImages.setAdapter(new InsertImageCategoryAdapter(getActivity(),
+				INNER_CATEGORIES));
+
 		gridView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
 				if (position == 4) {
-					Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+					// Intent i = new Intent(Intent.ACTION_PICK,
+					// android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+					// getActivity().startActivityForResult(i,
+					// RESULT_LOAD_IMAGE);
+				} else {
+					flipper.showNext();
+				}
+			}
+		});
+
+		gridViewImages.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				if (position == 4) {
+					Intent i = new Intent(
+							Intent.ACTION_PICK,
+							android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 					getActivity().startActivityForResult(i, RESULT_LOAD_IMAGE);
 				} else {
-					ImageView imageView = (ImageView) v.findViewById(R.id.grid_item_image);
+					ImageView imageView = (ImageView) v
+							.findViewById(R.id.grid_item_image);
 					imageView.buildDrawingCache();
-					Bitmap bmap = Bitmap.createBitmap(imageView.getDrawingCache());
+					Bitmap bmap = Bitmap.createBitmap(imageView
+							.getDrawingCache());
 					listener.onDialogPositiveClickInsertImageDialogListener(bmap);
 				}
 				getDialog().dismiss();
 			}
 		});
-		
+
 		// termina codigo prueba
-		builder.setView(gridViewContainer).setTitle(R.string.insert_image_title)
+		builder.setView(gridViewContainer)
+				.setTitle(R.string.insert_image_title)
+				.setNeutralButton("Volver",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								//flipper.showPrevious();
+							}
+						})
 				.setNegativeButton(R.string.insert_image_cancel,
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog, int id) {
-								dialog.dismiss();
+								//dialog.dismiss();
 							}
 						});
 		return builder.create();
