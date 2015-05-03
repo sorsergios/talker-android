c858cfe4e72c4a4fcbbf94ede07d1d86e3f0ecbb
alestarss <alejandra.stamato@gmail.com>
2015-05-03 18:46:32 -0300

    [MISC] reformateo

--- src/ar/uba/fi/talker/fragment/TalkerDialogFragment.java
+++ src/ar/uba/fi/talker/fragment/TalkerDialogFragment.java
@@ -12,28 +12,26 @@ public class TalkerDialogFragment extends DialogFragment {
 
 	@Override
 	public void onStart() {
-	    super.onStart();
-	    //Buttons
-	    Button positiveButton =  ((AlertDialog) getDialog()).getButton(DialogInterface.BUTTON_POSITIVE);
-	    Drawable drawablePositive = getActivity().getResources().getDrawable(R.drawable.accept);
-	    positiveButton.setText("");    
-	    positiveButton.setTextSize(0);
-	    positiveButton.setGravity(Gravity.CENTER);
-	    drawablePositive.setBounds(0,0, drawablePositive.getIntrinsicWidth(),
-                drawablePositive.getIntrinsicHeight());
-	            
-        positiveButton.setCompoundDrawables(null, drawablePositive,null, null);
-        positiveButton.setBackgroundColor(getResources().getColor(R.color.greenok));
-	    
-        Button negativeButton =  ((AlertDialog) getDialog()).getButton(DialogInterface.BUTTON_NEGATIVE);
-        negativeButton.setText("");    
-        negativeButton.setTextSize(0);
-        negativeButton.setGravity(Gravity.CENTER);
-	    Drawable drawableNegative = getActivity().getResources().getDrawable(R.drawable.cancel_back);
-        drawableNegative.setBounds(0,0, drawableNegative.getIntrinsicWidth(),drawableNegative.getIntrinsicHeight());
-        negativeButton.setCompoundDrawables(null, drawableNegative, null, null);
-        negativeButton.setBackgroundColor(getResources().getColor(R.color.redcancel));
+		super.onStart();
+		// Buttons
+		Button positiveButton = ((AlertDialog) getDialog()).getButton(DialogInterface.BUTTON_POSITIVE);
+		Drawable drawablePositive = getActivity().getResources().getDrawable(R.drawable.accept);
+		positiveButton.setText("");
+		positiveButton.setTextSize(0);
+		positiveButton.setGravity(Gravity.CENTER);
+		drawablePositive.setBounds(0, 0, drawablePositive.getIntrinsicWidth(), drawablePositive.getIntrinsicHeight());
+		positiveButton.setCompoundDrawables(null, drawablePositive, null, null);
+		positiveButton.setBackgroundColor(getResources().getColor(R.color.greenok));
+
+		Button negativeButton = ((AlertDialog) getDialog()).getButton(DialogInterface.BUTTON_NEGATIVE);
+		Drawable drawableNegative = getActivity().getResources().getDrawable(R.drawable.cancel_back);
+		negativeButton.setText("");
+		negativeButton.setTextSize(0);
+		negativeButton.setGravity(Gravity.CENTER);
+		drawableNegative.setBounds(0, 0, drawableNegative.getIntrinsicWidth(), drawableNegative.getIntrinsicHeight());
+		negativeButton.setCompoundDrawables(null, drawableNegative, null, null);
+		negativeButton.setBackgroundColor(getResources().getColor(R.color.redcancel));
 
 	}
-	
+
 }
